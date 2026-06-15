042be9b9cf4581f72a7b052ac355ad324eb3ac33
engrwithhabeeb <174087635+engrwithhabeeb@users.noreply.github.com>
2026-06-15 12:01:31 +0100

    feat(server): serve HTML files from disk with 500 error handling

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import http from "http";
+import fs from "fs";
 
 const routes = {
   "/": "index.html",
@@ -6,12 +7,25 @@ const routes = {
   "/contact_me": "contact_me.html",
 };
 
+function sendFile(fileName, callBack) {
+  fs.readFile(fileName, callBack);
+}
 const server = http.createServer((req, res) => {
   let fileName = routes[req.url];
+
   if (!fileName) {
     res.statusCode = 404;
     fileName = "404.html";
   }
+
+  sendFile(fileName, (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      res.end("Internal server error");
+      return;
+    }
+    res.end(data);
+  });
 });
 
 server.listen(8080, () => {
