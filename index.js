7de6a26bcfc5c079c7eb6681f1fbd8a7e36e2822
engrwithhabeeb <174087635+engrwithhabeeb@users.noreply.github.com>
2026-06-15 09:16:01 +0100

    feat(server): add static page routing map and 404 fallback logic

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import http from "http";
 
-const server = http.createServer((req, res) => {
+const routes = {
+  "/": "index.html",
+  "/about": "about.html",
+  "/contact_me": "contact_me.html",
+};
 
+const server = http.createServer((req, res) => {
+  let fileName = routes[req.url];
+  if (!fileName) {
+    res.statusCode = 404;
+    fileName = "404.html";
+  }
 });
 
 server.listen(8080, () => {
   console.log("Server running at localhost:8080");
 });
-
